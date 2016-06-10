f4a96c3ba8159e372a7ee98cb3171d7a662e3684
Chie <akiba.chie@gmail.com>
2016-06-10 14:04:43 -0700

    Enabled beer results function to show API results. Had to delete display none from .breweryResults in css. Also added hide and show method to the ul results

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var userCitySearch;
 var userStateSearch;
 var beerSearch;
 
+
 //API function for brewery request
 function getBreweryRequest(city, state) {
     userCitySearch = $('.cityInput').val();
@@ -15,17 +16,17 @@ function getBreweryRequest(city, state) {
     url = 'http://api.brewerydb.com/v2/locations';
     $.getJSON(url, params, function(data) {
         console.log(data);
-        showBreweryResults(data.items);
+        showBreweryResults(data.data);
     })
 }
 
-function showBreweryResults(items) {
-    var breweryName = name; 
-    $.each(items, function(index, item){
+function showBreweryResults(data) {
+    var breweryInfo = '';
+    $.each(data, function(index, item) {
         console.log(item);
-        breweryName += name;
+        breweryInfo += "<li class='nameBrewery'>Name: " + item.brewery.name + "</li>" + "<li class='street-addressBrewery'>Street Address: " + item.streetAddress + "</li>" + "<li class='cityBrewery'>City: " + item.locality + "</li>" + "<li class='stateBrewery'>State: " + item.region + "</li>" + "<li class='zipcodeBrewery'>Zip Code: " + item.postalCode + "</li>" + "<li class='phoneBrewery'>Phone number: " + item.phone + "</li>" + "<li class='websiteBrewery'>Website: " + item.website + "</li>" + "<li class='hoursBrewery'>Hours of Operation: " + item.hoursOfOperation + "</li>" + "<li class='tourBrewery'>Tour info: " + item.tourInfo + "</li>"
     })
-    $('.nameBrewery').append(breweryName);
+    $('.breweryResults').append(breweryInfo);
 
 }
 
@@ -45,9 +46,9 @@ function getBeerRequest(name) {
 
 function showBeerResults(data) {
     var beerInfo = '';
-    $.each(data, function(index, item){
+    $.each(data, function(index, item) {
         console.log(item);
-        beerInfo +=  '<li class="nameBeer">Name: ' + item.name + '</li>' + '<li class="abvBeer">ABV: ' + item.abv + '</li>' + '<li class="foodPairingBeer">Best food to pair with: ' + item.foodPairings + '</li>' + '<li class="descriptionBeer">Description: ' + item.description + '</li>'
+        beerInfo += '<li class="nameBeer">Name: ' + item.name + '</li>' + '<li class="abvBeer">ABV: ' + item.abv + '</li>' + '<li class="foodPairingBeer">Best food to pair with: ' + item.foodPairings + '</li>' + '<li class="descriptionBeer">Description: ' + item.description + '</li>'
     });
     $('.beerResults').append(beerInfo);
 }
@@ -70,12 +71,16 @@ function showMapResults(items) {
 }
 
 $(document).ready(function() {
+    $('.breweryResults').hide();
+    $('.beerResults').hide();
     $('.searchBrewery').click(function(event) {
         event.preventDefault();
         getBreweryRequest(userCitySearch, userStateSearch);
+        $('.breweryResults').show();
     });
     $('.searchBeer').click(function(event) {
         event.preventDefault();
         getBeerRequest(beerSearch);
+        $('.beerResults').show();
     })
 })
