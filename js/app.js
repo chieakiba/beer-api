8d5ac4761728524c29215dfeffae70ed9f59d721
Chie <akiba.chie@gmail.com>
2016-06-10 20:06:55 -0700

    Initial commit for the beer icons in the image folder. Updated css styles so the map and brewery results would fit in one page. Also added a line between every entry so it's easier to see the results

--- js/app.js
+++ js/app.js
@@ -2,33 +2,7 @@
 var userCitySearch;
 var userStateSearch;
 var beerSearch;
-
-
-//API function for brewery request
-function getBreweryRequest(city, state) {
-    userCitySearch = $('.cityInput').val();
-    userStateSearch = $('.stateInput').val();
-    var params = {
-        key: '4eee5db4403b144dbc068306c3d4680f',
-        locality: userCitySearch,
-        region: userStateSearch
-    }
-    url = 'http://api.brewerydb.com/v2/locations';
-    $.getJSON(url, params, function(data) {
-        console.log(data);
-        showBreweryResults(data.data);
-    })
-}
-
-function showBreweryResults(data) {
-    var breweryInfo = '';
-    $.each(data, function(index, item) {
-        console.log(item);
-        breweryInfo += "<li class='nameBrewery'>Name: " + item.brewery.name + "</li>" + "<li class='street-addressBrewery'>Street Address: " + item.streetAddress + "</li>" + "<li class='cityBrewery'>City: " + item.locality + "</li>" + "<li class='stateBrewery'>State: " + item.region + "</li>" + "<li class='zipcodeBrewery'>Zip Code: " + item.postalCode + "</li>" + "<li class='phoneBrewery'>Phone number: " + item.phone + "</li>" + "<li class='websiteBrewery'>Website: " + item.website + "</li>" + "<li class='hoursBrewery'>Hours of Operation: " + item.hoursOfOperation + "</li>" + "<li class='tourBrewery'>Tour info: " + item.tourInfo + "</li>"
-    })
-    $('.breweryResults').append(breweryInfo);
-
-}
+var map;
 
 //API function for beer request
 function getBeerRequest(name) {
@@ -48,26 +22,56 @@ function showBeerResults(data) {
     var beerInfo = '';
     $.each(data, function(index, item) {
         console.log(item);
-        beerInfo += '<li class="nameBeer">Name: ' + item.name + '</li>' + '<li class="abvBeer">ABV: ' + item.abv + '</li>' + '<li class="foodPairingBeer">Best food to pair with: ' + item.foodPairings + '</li>' + '<li class="descriptionBeer">Description: ' + item.description + '</li>'
+        beerInfo += '<hr><li class="nameBeer">Name: ' + item.name + '</li>' + '<li class="abvBeer">ABV: ' + item.abv + '</li>' + '<li class="foodPairingBeer">Best food to pair with: ' + item.foodPairings + '</li>' + '<li class="descriptionBeer">Description: ' + item.description + '</li></hr>'
     });
     $('.beerResults').append(beerInfo);
 }
 
-//Google Maps API
-function mapRequest() {
+//API function for brewery request
+function getBreweryRequest(city, state) {
+    userCitySearch = $('.cityInput').val();
+    userStateSearch = $('.stateInput').val();
     var params = {
-        key: 'AIzaSyC67XoBhkayUP2I11lJdcFB4TD3gFBBvs0',
-
+        key: '4eee5db4403b144dbc068306c3d4680f',
+        locality: userCitySearch,
+        region: userStateSearch
     }
-    url = 'http://maps.googleapis.com/';
+    url = 'http://api.brewerydb.com/v2/locations';
     $.getJSON(url, params, function(data) {
         console.log(data);
-        showMapResults(data.items);
+        showBreweryResults(data.data);
     })
 }
 
-function showMapResults(items) {
-
+function showBreweryResults(data) {
+    var breweryInfo = '';
+    var breweryLatitude = '';
+    var breweryLongitude = '';
+    var breweryName = '';
+    $.each(data, function(index, item) {
+        console.log(item);
+        breweryInfo += "<hr><li class='nameBrewery'>Name: " + item.brewery.name + "</li>" + "<li class='street-addressBrewery'>Street Address: " + item.streetAddress + "</li>" + "<li class='cityBrewery'>City: " + item.locality + "</li>" + "<li class='stateBrewery'>State: " + item.region + "</li>" + "<li class='zipcodeBrewery'>Zip Code: " + item.postalCode + "</li>" + "<li class='phoneBrewery'>Phone number: " + item.phone + "</li>" + "<li class='websiteBrewery'>Website: " + item.website + "</li>" + "<li class='hoursBrewery'>Hours of Operation: " + item.hoursOfOperation + "</li>" + "<li class='tourBrewery'>Tour info: " + item.tourInfo + "</li></hr>"
+        breweryLatitude += item.latitude;
+        breweryLongitude += item.longitude;
+        breweryName += item.brewery.name;
+    })
+    $('.breweryResults').append(breweryInfo);
+    //Google Maps API
+    function breweryMap() {
+        var myLatLng = {lat: breweryLatitude, lng: breweryLongitude};
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+        var beerIcon = 'image/beermarker.png'
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: breweryName,
+            icon: beerIcon
+        });
+    }
+    breweryMap();
 }
 
 $(document).ready(function() {
