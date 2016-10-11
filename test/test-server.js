5d3e21510876445c6d8a35da4a65c9f191ed7cc8
Chie <akiba.chie@gmail.com>
2016-10-10 17:10:49 -0700

    updatd test-server.js since we don't need to use chai testing yet

--- test/test-server.js
+++ test/test-server.js
@@ -1,16 +1,10 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var page = require('../public/index.html');
+var server = require('../server');
 
 var should = chai.should();
-var app = page.app;
+var app = server.app;
 
 describe('Find beer or brewery', function() {
-  it('should show the landing page', function(done) {
-    chai.request(app)
-      .end(function(err, res) {
-        res.should.have.status(200);
-        done();
-      });
-  });
+  it('should show the landing page');
 });
